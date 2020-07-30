ec75443acb40846a203882b031fe1f3cb66bd5f8
kaliberpoziomka <szubiczuk.krzysztof@gmail.com>
2020-07-30 08:42:50 +0200

    Updates

---
+++ precache-manifest.c5ae8b58fec4ad0e3553251d6d6f1d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb55434501a940ef0b5aa010f599dcf3",
+    "url": "/react-weatherapp/index.html"
+  },
+  {
+    "revision": "5fad5569e8503d65a393",
+    "url": "/react-weatherapp/static/css/main.4e6edc8d.chunk.css"
+  },
+  {
+    "revision": "4eab7b620f8c41771cfa",
+    "url": "/react-weatherapp/static/js/2.ed8eb188.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-weatherapp/static/js/2.ed8eb188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fad5569e8503d65a393",
+    "url": "/react-weatherapp/static/js/main.54dfa94a.chunk.js"
+  },
+  {
+    "revision": "d6cc8ec6d8c3bcf5429c",
+    "url": "/react-weatherapp/static/js/runtime-main.cecb34ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9ca5e8d39ff7b8a8408fd0309e2e117.js"
+  "/react-weatherapp/precache-manifest.c5ae8b58fec4ad0e3553251d6d6f1d75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-weatherapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
